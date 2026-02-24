1df4a665edd0b0cff8bef11deeeb6cde0046aa47
Johan Steen Thomassen <johan@Johans-MacBook-Pro-2.local>
2026-02-24 12:05:47 +0100

    Galaga v.1.0
    Player added to Game.cs

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,24 +1,33 @@
 namespace Galaga;
 
+using System.Numerics;
 using DIKUArcade;
 using DIKUArcade.GUI;
 using DIKUArcade.Input;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
 public class Game : DIKUGame {
+        private Player player;
     public Game(WindowArgs windowArgs) : base(windowArgs) {
+        player = new Player(
+            new DynamicShape(new Vector2(0.45f, 0.1f),
+                            new Vector2(0.1f, 0.1f)),
+            new Image("Galaga.Assets.Images.Player.png"));
+
         // TODO: Set key event handler (inherited window field of DIKUGame class)
 
     }
 
     public override void Render(WindowContext context) {
-        throw new System.NotImplementedException("Galaga game has nothing to render yet.");
+        player.RenderEntity(context);
     }
 
     public override void Update() {
-        throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+        // Code here
     }
 
     public override void KeyHandler(KeyboardAction action, KeyboardKey key) {
-        throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+        // Code here
     }
 
 }
