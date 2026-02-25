1c1718f777b0f272f0f6b46e6be2e0d86f2b0353
Johan Steen Thomassen <johan@Johans-MacBook-Pro-2.local>
2026-02-25 11:03:44 +0100

    5.2.3 færdiggjort. Fungerer.
    Nu kan vi se 8 enemies. Indeholder midlertidige forklarende noter.

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,5 +1,6 @@
 namespace Galaga;
 
+using System.Collections.Generic;
 using System.Numerics;
 using DIKUArcade;
 using DIKUArcade.GUI;
@@ -8,16 +9,31 @@ using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 public class Game : DIKUGame {
         private Player player;
+        private EntityContainer<Enemy> enemies;
     public Game(WindowArgs windowArgs) : base(windowArgs) {
         player = new Player(
             new DynamicShape(new Vector2(0.45f, 0.1f),
                             new Vector2(0.1f, 0.1f)),
             new Image("Galaga.Assets.Images.Player.png"));
+        
+        //5.2.3 Enemies: Animation
+        List<Image> images =
+            ImageStride.CreateStrides(4, "Galaga.Assets.Images.BlueMonster.png"); //ImageStride (ligger i Graphics) får dem til at bevæge sig. 4 henviser til antallet af frames i image-filen. The string er der hvor images bor.
+        const int numEnemies = 8;
+        enemies = new EntityContainer<Enemy>(numEnemies); //Entitycontainer kan vist ses som en liste af entities. Tager int size.
+        for (int i = 0; i < numEnemies; i++) {
+            enemies.AddEntity(new Enemy( // Her tilføjes enemy entities til EntetyContaineren en ad gangen til vi når i-1.
+                new DynamicShape(new Vector2(0.1f + (float)i * 0.1f, 0.9f), // Her placeres en enemy på skærmen. i sørger for, at de ikke ligger oveni hinanden.
+                                new Vector2(0.1f, 0.1f)),
+                new ImageStride(80, images))); // Her sættes en ImageStride: public ImageStride(int milliseconds, IEnumerable<Image> images) 
+        }
+
         // TODO: Set key event handler (inherited window field of DIKUGame class)
     }
 
     public override void Render(WindowContext context) {
         player.RenderEntity(context);
+        enemies.RenderEntities(context); // RenderEnteties ligger i EntityContainer.cs.
     }
 
     public override void Update() {
